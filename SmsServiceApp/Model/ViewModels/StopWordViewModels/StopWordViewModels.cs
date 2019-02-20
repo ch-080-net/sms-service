3031d043acb76442f7aa458d0c1c283423bd9fae
Valentin7arema <44587140+Valentin7arema@users.noreply.github.com>
2019-02-21 01:32:18 +0200

    #16 Add StopWord repository+break controller(need map)+empty View

---
+++ SmsServiceApp/Model/ViewModels/StopWordViewModels/StopWordViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.ViewModels.StopWordViewModels
+{
+    public class StopWordViewModels
+    {
+        public string Word { get; set; }
+
+    }
+}
---
+++ SmsServiceApp/WebCustomerApp/Controllers/StopWordController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Model.Interfaces;
+using Model.ViewModels.StopWordViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebCustomerApp.Controllers;
+using WebCustomerApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class StopWordController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        
+        public StopWordController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        [HttpGet]
+
+        public IActionResult AddWord(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddWord(StopWordViewModels model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var word = new StopWord { Word=model.Word };
+          /*      var result = await _unitOfWork.StopWords.Create(word);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToLocal(returnUrl);
+                }
+                AddErrors(result);*/
+            }
+
+            return View(model);
+        }
+
+      
+
+        [HttpGet]
+        public IActionResult ResetWord(string code = null)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ResetWord(ResetStopWordViewModels model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+       /*     var word = await _unitOfWork.StopWords.SearchByWord(model.PriorWord);
+            if (word == null)
+            {
+            var result = await _unitOfWork.StopWords.Update(word);
+            }
+        
+            if (result.Succeeded)
+            {
+               return RedirectToAction(nameof(returnUrl));
+            }
+            AddErrors(result);*/
+            return View();
+        }
+
+
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+        }
+    }
+}
