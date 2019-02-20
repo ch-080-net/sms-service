f078307adfbb4caf09aef24cd856e48368d590c3
Pasha Ivasiuta <pash.ok13@yandex.ua>
2019-02-20 22:38:04 +0200

    #15 add company interface and repository, add method for validation on adding new receivers and method for getting limit tariff

---
+++ SmsServiceApp/Model/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebCustomerApp.Models;
+
+namespace Model.Interfaces
+{
+	public interface ICompanyRepository : IBaseRepository<Company>
+	{
+		bool IsLimitExceeded(int messageCount, Company currentCompany);
+		int GetTariffLimit(Company company);
+	}
+}
