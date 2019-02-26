582a8b3d427a7ba75a3e21671dc83a73c243aa4e
Alexander Rozhdov <rozhdov@outlook.com>
2019-02-26 17:40:52 +0200

    #27 #28

--- SmsServiceApp/Model/DB/Operator.cs
+++ SmsServiceApp/Model/DB/Operator.cs
@@ -4,16 +4,18 @@ using System.Text;
 
 namespace WebCustomerApp.Models
 {
+    /// <summary>
+    /// Name - for name of cell service provider, such as Vodafone
+    /// Logo - contains logo of cell service provider
+    /// </summary>
     public class Operator
     {
         public int Id { get; set; }
         public ICollection<Code> Codes { get; set; }
         public ICollection<Tariff> Tariffs { get; set; }
 
-        // Name of cell service provider, such as Vodafone
         public string Name { get; set; }
 
-        // Contains logo (as reference right now)
         public byte[] Logo { get; set; }
     }
 }
