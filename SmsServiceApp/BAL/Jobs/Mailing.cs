8e4dd2b8cd5e6eeeea559662041ca9035f262351
Alexander Rozhdov <rozhdov@outlook.com>
2019-03-01 16:20:36 +0200

    #22 Initial work on quartz.net implementation

---
+++ SmsServiceApp/BAL/Jobs/Mailing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace BAL.Jobs
+{
+    public class Mailing : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            
+        }
+    }
+}
---
+++ SmsServiceApp/BAL/Jobs/MailingScheduler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Quartz;
+using Quartz.Impl;
+using System.Threading.Tasks;
+using Model.Interfaces;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BAL.Jobs
+{
+    public class MailingScheduler
+    {
+        public static async void Start(IServiceScopeFactory scopeFactory)
+        {
+            IScheduler scheduler = await StdSchedulerFactory.GetDefaultScheduler();
+            await scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<Mailing>().Build();
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("MailingTrigger", "default")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                .WithIntervalInMinutes(5)
+                .RepeatForever())
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
