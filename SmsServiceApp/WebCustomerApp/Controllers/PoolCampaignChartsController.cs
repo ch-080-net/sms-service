7eec45c683df265a2d5d316921131df83b832f44
Alexander Rozhdov <rozhdov@outlook.com>
2019-03-12 13:27:18 +0200

    #37 interim work

---
+++ SmsServiceApp/WebCustomerApp/Controllers/PoolCampaignChartsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Model.ViewModels.CodeViewModels;
+using BAL.Managers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using Model.Interfaces;
+using Newtonsoft.Json;
+
+namespace WebApp.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class PoolCampaignChartsController : Controller
+    {
+        
+    }
+}
