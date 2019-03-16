e9b0c32a5cdc5a12febb13769398deb47c29d6b3
Pasha Ivasiuta <pash.ok13@yandex.ua>
2019-03-16 14:35:08 +0200

    #21 #34 refactoring, add summaries, bug fixes

--- SmsServiceApp/BAL/Services/SmsSender.cs
+++ SmsServiceApp/BAL/Services/SmsSender.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-using System.ServiceProcess;
 using System.IO;
 using Model.Interfaces;
 using System.Linq;
@@ -22,61 +21,88 @@ namespace WebCustomerApp.Services
 	public class SmsSender : ISmsSender 
 	{
 		public SMSCclientSMPP clientSMPP;
-		public List<uint> messageIDs;
+		public List<uint> messageNumbers;
 
         private ICollection<MessageDTO> messagesForSend = new List<MessageDTO>();
         private IServiceScopeFactory serviceScopeFactory;
 
-        public SmsSender(IServiceScopeFactory serviceScopeFactory)
+		/// <summary>
+		/// Create SMPP client, add event handlers 
+		/// for getting information about received and sended messages,
+		/// create connection and initialize session
+		/// </summary>
+		/// <param name="KeepAliveInterval">Property for changing connection time(seconds) with server</param>
+		/// <param name="serviceScopeFactory">instance of static service</param>
+		public SmsSender(IServiceScopeFactory serviceScopeFactory)
 		{
             this.serviceScopeFactory = serviceScopeFactory;
 			clientSMPP = new SMSCclientSMPP();
 			clientSMPP.KeepAliveInterval = 60;
-			messageIDs = new List<uint>();
 			clientSMPP.OnSmppMessageReceived += SMSCclientSMPP_OnSmppMessageReceived;
 			clientSMPP.OnSmppStatusReportReceived += SMSCclientSMPP_OnSmppStatusReportReceived;
+			clientSMPP.OnSmppSubmitResponseAsyncReceived += SMSCclientSMPP_OnSmppSubmitResponseAsyncReceived;
 
 			Connect();
-			OpenSession();
 		}
 
-        /// <summary>
-        /// Sets the connection with emulator,
-        /// If you want connect to server, you should change remote host
-        /// </summary>
-        /// <returns>True - if the connection is established</returns>
-        private async Task Connect()
+		#region Connection methods
+		/// <summary>
+		/// Sets the connection with SMPP Server,
+		/// If you want connect to server, you should change remote host
+		/// Ends when establishes the connection
+		/// </summary>
+		private async Task Connect()
 		{
             int connectionStatus;
             do
             {
-                connectionStatus = clientSMPP.tcpConnect("127.0.0.1", 2775, ""); 				
-                if (connectionStatus == 0)
-                    break;
-                else
-                    await Task.Delay(5000);
+                connectionStatus = clientSMPP.tcpConnect("127.0.0.1", 2775, "");
+				if (connectionStatus == 0)
+				{
+					OpenSession();
+					break;
+				}
+				else
+				{
+					await Task.Delay(5000);
+				}
             } while (true);
-
 		}
 
 		/// <summary>
 		/// Open session for static default user, if you want change user parameters
-		/// You should add another user in smppsim.props file
+		/// you should add another user in smppsim.props file
+		/// Throw exception when you try connect with incorrect system id or password
 		/// </summary>
-		/// <returns>True - if the session are opened</returns>
-		private async Task OpenSession()
+		private void OpenSession()
 		{
-			int sessionStatus;
-            do
-            {
-                sessionStatus = clientSMPP.smppInitializeSessionEx("smppclient1", "password", 1, 1, "", smppBindModeEnum.bmTransceiver, 3, "");          
-                if (sessionStatus == 0)
-                    break;
-                else
-					await Task.Delay(5000);
-            } while (true);
+			int sessionStatus = clientSMPP.smppInitializeSessionEx("smppclient1", "password", 1, 1, "", smppBindModeEnum.bmTransceiver, 3, "");
+
+			if (sessionStatus != 0)
+				throw new Exception("Invalid SMPP connection data");
         }
 
+		/// <summary>
+		/// Close current session
+		/// </summary>
+		/// <returns>True - if session are closet correctly</returns>
+		//private void CloseSession()
+		//{
+		//	try { clientSMPP.smppFinalizeSession(); }
+		//	finally { }
+		//}
+
+		/// <summary>
+		/// Close connection with service
+		/// </summary>
+		//private void Disconnect()
+		//{
+		//	try { clientSMPP.tcpDisconnect(); }
+		//	finally { }
+		//}
+		#endregion
+
+		#region Message sending
 		/// <summary>
 		/// Send collection of messages 
 		/// </summary>
@@ -84,10 +110,7 @@ namespace WebCustomerApp.Services
 		public async Task SendMessages(IEnumerable<MessageDTO> messages)
 		{
 			if (!clientSMPP.Connected)
-			{
 				await Connect();
-				await OpenSession();
-			}
 
 			foreach (MessageDTO message in messages)
 				SendMessage(message);
@@ -95,8 +118,9 @@ namespace WebCustomerApp.Services
 
 		/// <summary>
 		/// Send message from one user for one recepient with received text
-		/// You should transfer user and recepient phone in format +xxxxxxxxxxxx
-		/// Throw exception with user and recepient phones if message aren`t sended
+		/// you should transfer user and recepient phone in format +xxxxxxxxxxxx
+		/// messages are sending in async mode
+		/// when the result status = -1 server id is added in later, and we receive a message	
 		/// </summary>
 		/// <param name="message">Message for send</param>
 		public void SendMessage(MessageDTO message)
@@ -110,34 +134,21 @@ namespace WebCustomerApp.Services
 
 			int resultStatus = clientSMPP.smppSubmitMessageAsync(message.RecepientPhone, 1, 1, message.SenderPhone, 1, 1,
 							message.MessageText, EncodingEnum.etUCS2Text, "", options, 
-							DateTime.Now, DateTime.Now, "", 0, "", out messageIDs);
+							DateTime.Now, DateTime.Now, "", 0, "", out messageNumbers);
 
-			//when the status = -1 server id is added in later, and we receive a message			
 			if (resultStatus == 0)
-				message.ServerId = Convert.ToString(messageIDs.FirstOrDefault()-2);
+				message.SequenceNumber = messageNumbers.FirstOrDefault();
 			else if (resultStatus != -1)
 				messagesForSend.Remove(message);
 		}
-		
-		/// <summary>
-		/// Close current session
-		/// </summary>
-		/// <returns>True - if session are closet correctly</returns>
-		private void CloseSession()
-		{
-            try { clientSMPP.smppFinalizeSession(); }
-            finally { }
-		}
+		#endregion
 
+		#region Events
 		/// <summary>
-		/// Close connection with service
+		/// Add information about incoming messages to file
 		/// </summary>
-		private void Disconnect()
-		{
-            try { clientSMPP.tcpDisconnect(); }
-            finally { }
-		}
-
+		/// <param name="sender">sender</param>
+		/// <param name="e">event object</param>
 		private void SMSCclientSMPP_OnSmppMessageReceived(object sender, smppMessageReceivedEventArgs e)
 		{
 			string report = $"Message From: {e.Originator}, To: {e.Destination}, Text: {e.Content}";
@@ -148,47 +159,80 @@ namespace WebCustomerApp.Services
 			}
 		}
 
-        // Status Report (SR) received from SMSC
-        private void SMSCclientSMPP_OnSmppStatusReportReceived(object sender, smppStatusReportReceivedEventArgs e)
-        {
+		/// <summary>
+		/// Add message id from SMPP according to sequence number
+		/// </summary>
+		/// <param name="Sender">sender</param>
+		/// <param name="e">event object</param>
+		private void SMSCclientSMPP_OnSmppSubmitResponseAsyncReceived(object Sender, smppSubmitResponseAsyncReceivedEventArgs e)
+		{
+			MessageDTO message;
+			do
+				message = messagesForSend.FirstOrDefault(s => s.SequenceNumber == e.SequenceNumber);
+			while (message == null);
+			
+			message.ServerId = e.MessageID;
+		}
+
+		/// <summary>
+		/// Add information about outloging messages in file
+		/// </summary>
+		/// <param name="sender">sender</param>
+		/// <param name="e">event object</param>
+		private void SMSCclientSMPP_OnSmppStatusReportReceived(object sender, smppStatusReportReceivedEventArgs e)
+		{
 			string report = $"Message From: {e.Originator}, To: {e.Destination}, Content: {e.Content}";
 
 			using (StreamWriter sw = new StreamWriter(@"Log.txt", true, Encoding.UTF8))
-            {
-                sw.WriteLine(report);
-            }
+				sw.WriteLine(report);
 
-            if (e.NetworkErrorCode == 0)
-            {
-                MessageState messageState;
-                switch (e.MessageState)
-                {
-                    case 2:
-                        messageState = MessageState.Delivered;
-                        break;
-                    case 6:
-                        messageState = MessageState.Accepted;
-                        break;
-                    case 5:
-                        messageState = MessageState.Undeliverable;
-                        break;
-                    case 8:
-                        messageState = MessageState.Rejected;
-                        break;
-                    default:
-                        return;
-                }
-                var temp = messagesForSend.FirstOrDefault(m => m.ServerId == e.MessageID);
-                if (temp != null)
-                {
-                    using (var scope = serviceScopeFactory.CreateScope())
-                    {
-                        scope.ServiceProvider.GetService<IMailingManager>().MarkAs(temp, messageState);
-                    }
-                    messagesForSend.Remove(temp);
-                }
-            }
-        }
+			if (e.NetworkErrorCode == 0)
+				ChangeMessageState(e.MessageState, e.MessageID);
+		}
+		#endregion
+
+		#region Support functions
+		/// <summary>
+		/// Change message state in database
+		/// throw exception when couldn`t find information about report object
+		/// in current message collection
+		/// </summary>
+		/// <param name="messageStateCode">message state code from report object</param>
+		/// /// <param name="messageId">message id from SMPP server</param>
+		private void ChangeMessageState(int messageStateCode, string messageId)
+		{
+			MessageState messageState;
+			switch (messageStateCode)
+			{
+				case 2:
+					messageState = MessageState.Delivered;
+					break;
+				case 6:
+					messageState = MessageState.Accepted;
+					break;
+				case 5:
+					messageState = MessageState.Undeliverable;
+					break;
+				case 8:
+					messageState = MessageState.Rejected;
+					break;
+				default:
+					return;
+			}
+			MessageDTO temp = messagesForSend.FirstOrDefault(m => m.ServerId == messageId);
+			if (temp != null)
+			{
+				using (var scope = serviceScopeFactory.CreateScope())
+				{
+					scope.ServiceProvider.GetService<IMailingManager>().MarkAs(temp, messageState);
+				}
+				messagesForSend.Remove(temp);
+			}
+			else
+			{
+				throw new Exception("Report for unknown message");
+			}
+		}
+		#endregion
 	}
-	
 }
