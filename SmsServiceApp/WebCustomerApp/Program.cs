e2d9bd3422e53309652bcb3a107a489cbf4e51ee
Nick Tkachuk <tmikola11@gmail.com>
2019-03-18 16:29:44 +0200

    ApplyBootstrapThemeAndCreateMenu#20

--- SmsServiceApp/WebCustomerApp/Program.cs
+++ SmsServiceApp/WebCustomerApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BAL.Services;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -10,7 +11,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using WebCustomerApp.Models;
-using static WebCustomerApp.Startup;
 
 namespace WebCustomerApp
 {
@@ -29,7 +29,7 @@ namespace WebCustomerApp
 
                     var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
-                    MyIdentityDataInitializer.SeedData(userManager, roleManager);
+                    IdentityDataInitializer.SeedData(userManager, roleManager);
                 }
                 catch
                 {
