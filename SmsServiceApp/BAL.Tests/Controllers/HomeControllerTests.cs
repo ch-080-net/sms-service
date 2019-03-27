8d6c6373352c5edb33665b8334ae288ffd24bcfc
Pasha Ivasiuta <pash.ok13@yandex.ua>
2019-03-27 17:10:20 +0200

    Add project for testing web part

---
+++ SmsServiceApp/BAL.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApp.Controllers;
+using WebApp.Models.AccountViewModels;
+
+namespace WEB.Tests.Controllers
+{
+	[TestFixture]
+	public class HomeControllerTests
+	{
+		[TestCase]
+		public void Lockout()
+		{
+			//var mock = new Mock<IStringLocalizer>();
+			//HomeController controller = new HomeController(mock);
+			//// Act
+			//ViewResult result = controller.Lockout() as ViewResult;
+			//// Assert
+			Assert.AreEqual("nice","nice");
+		}
+	}
+}
