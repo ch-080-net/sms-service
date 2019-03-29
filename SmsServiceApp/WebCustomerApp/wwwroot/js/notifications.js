8d04c4ed068553c7bfbee9c60c09ad3b85c5eb59
Alexander Rozhdov <rozhdov@outlook.com>
2019-03-29 12:46:11 +0300

    #49 Added notifications button, modal window, initial work on JS to suplement them

--- SmsServiceApp/WebCustomerApp/wwwroot/js/notifications.js
+++ SmsServiceApp/WebCustomerApp/wwwroot/js/notifications.js
@@ -8,9 +8,30 @@ connection.on("GetNotification", function (notification) {
     if (storedNotifications == null) {
         storedNotifications = [];
     }
+    else if (storedNotifications.length > 10) {
+        storedNotifications = storedNotifications.slice(storedNotifications.length - 9);
+    }
+
     storedNotifications.push(notification);
     storedNotifications = JSON.stringify(storedNotifications);
     localStorage.setItem("notifications", storedNotifications);
 });
 
 connection.start();
+
+function fillModal() {
+
+    var content;
+    var storedNotifications = localStorage.getItem("notifications");
+    storedNotifications = JSON.parse(storedNotifications);
+    if (storedNotifications == null)
+        return;
+    for (var i = 0; i < storedNotifications.length; i++) {
+        content += "<h4>" + storedNotifications[i].Title + "</h4>" + "<small>" + storedNotifications.Time + "</small>";
+        content += "<p>" + storedNotifications[i].Message + "</p>";
+        content += "<br />";
+    }
+
+    document.getElementById("notificationContent").innerHTML = content;
+}
+
