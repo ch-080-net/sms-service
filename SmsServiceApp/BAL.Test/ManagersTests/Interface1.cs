0a70ed28dd43dc6ca6a07389ce19bfe0170a3984
Valik-Yarema <34391973+Valik-Yarema@users.noreply.github.com>
2019-04-12 15:44:19 +0300

    + baseTests first

---
+++ SmsServiceApp/BAL.Test/ManagersTests/Interface1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BAL.Test.ManagersTests
+{
+   public interface IBaseTests<TEntity> where TEntity : class
+    {
+    }
+}
