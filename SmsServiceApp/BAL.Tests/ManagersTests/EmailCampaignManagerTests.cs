9a3709b201646f2409bafa2f84831da9097acae0
ParaschukOleksii <47175783+ParaschukOleksii@users.noreply.github.com>
2019-04-15 16:32:43 +0300

    Add some test

---
+++ SmsServiceApp/BAL.Tests/ManagersTests/EmailCampaignManagerTests.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using BAL.Managers;
+using BAL.Notifications.Infrastructure;
+using Model.ViewModels.EmailCampaignViewModels;
+using Model.ViewModels.EmailRecipientViewModels;
+using NUnit.Framework;
+using Moq;
+using WebApp.Models;
+using System.Linq;
+using System.Linq.Expressions;
+using WebApp.Services;
+
+namespace BAL.Tests.ManagersTests
+{
+    [TestFixture]
+    public class EmailCampaignManagerTests : TestInitializer
+    {
+        private static Mock<INotificationsGenerator<EmailCampaign>> mockNotification = new Mock<INotificationsGenerator<EmailCampaign>>();
+        private EmailCampaignManager manager = new EmailCampaignManager(mockUnitOfWork.Object, mockMapper.Object, mockNotification.Object);
+
+        [Test]
+        public void Insert_EmptyCampaign_FalseResult()
+        {
+            EmailCampaignViewModel emptyCampaign = new EmailCampaignViewModel();
+            List<EmailRecipientViewModel> recepientsList = new List<EmailRecipientViewModel>();
+            recepientsList.Add(new EmailRecipientViewModel()
+            {
+                EmailAddress = "tt@tt.tt",
+                BirthDate = DateTime.Now,
+                Gender = "Male",
+                KeyWords = "add",
+                Name = "John",
+                Surname = "Snow",
+                Priority = "Low"
+            });
+            var result = manager.IncertWithRecepients(emptyCampaign, recepientsList);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Insert_CampignWithoutEmailInDatabase_TrueResult()
+        {            
+            EmailCampaignViewModel emptyCampaign = new EmailCampaignViewModel() { EmailAddress = "", Name = "test", Message = "test", };
+            List<EmailRecipientViewModel> recepientsList = new List<EmailRecipientViewModel>();
+            List<Email> Emails = new List<Email>();
+            Emails.Add(
+                new Email()
+                {
+                    Id = 10,
+                    EmailAddress = "qq@qq.qq"
+                });
+            Emails.Add(
+                new Email()
+                {
+                    Id = 11,
+                    EmailAddress = "tt@tt.tt"
+                });
+            mockUnitOfWork.Setup(u => u.Emails.Get(It.IsAny<Expression<Func<Email, bool>>>(), It.IsAny<Func<IQueryable<Email>,
+                IOrderedQueryable<Email>>>(), It.IsAny<string>())).Returns(new List<Email>());
+            mockUnitOfWork.Setup(u => u.EmailCampaigns.Insert(It.IsAny<EmailCampaign>()));
+            mockUnitOfWork.Setup(u => u.EmailRecipients.Insert(It.IsAny<EmailRecipient>()));
+            mockUnitOfWork.Setup(u => u.Emails.Insert(It.IsAny<Email>()));
+            mockMapper.Setup(m => m.Map<EmailCampaign>(It.IsAny<EmailCampaignViewModel>()))
+                .Returns(new EmailCampaign() {Name = "test", Message = "test"});
+            recepientsList.Add(new EmailRecipientViewModel()
+            {
+                EmailAddress = "tt@tt.tt",
+                BirthDate = DateTime.Now,
+                Gender = "Male",
+                KeyWords = "add",
+                Name = "John",
+                Surname = "Snow",
+                Priority = "Low"
+            });
+            mockMapper.Setup(m => m.Map<EmailRecipientViewModel, EmailRecipient>(It.IsAny<EmailRecipientViewModel>()))
+                .Returns(new EmailRecipient() { BirthDate = DateTime.Now, Gender = 0, KeyWords = "add", Name = "John", Surname = "Snow", Priority = "Low"});
+            var result = manager.IncertWithRecepients(emptyCampaign, recepientsList);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Insert_CampignWithEmailInDatabase_TrueResult()
+        {
+            EmailCampaignViewModel emptyCampaign = new EmailCampaignViewModel() { EmailAddress = "", Name = "test", Message = "test", };
+            List<EmailRecipientViewModel> recepientsList = new List<EmailRecipientViewModel>();
+            List<Email> Emails = new List<Email>();
+            Emails.Add(
+                new Email()
+                {
+                    Id = 10,
+                    EmailAddress = "qq@qq.qq"
+                });
+            Emails.Add(
+                new Email()
+                {
+                    Id = 11,
+                    EmailAddress = "tt@tt.tt"
+                });
+            mockUnitOfWork.Setup(u => u.Emails.Get(It.IsAny<Expression<Func<Email, bool>>>(), It.IsAny<Func<IQueryable<Email>,
+                IOrderedQueryable<Email>>>(), It.IsAny<string>())).Returns(Emails);
+            mockUnitOfWork.Setup(u => u.EmailCampaigns.Insert(It.IsAny<EmailCampaign>()));
+            mockUnitOfWork.Setup(u => u.EmailRecipients.Insert(It.IsAny<EmailRecipient>()));
+            mockUnitOfWork.Setup(u => u.Emails.Insert(It.IsAny<Email>()));
+            mockMapper.Setup(m => m.Map<EmailCampaign>(It.IsAny<EmailCampaignViewModel>()))
+                .Returns(new EmailCampaign() { Name = "test", Message = "test" });
+            recepientsList.Add(new EmailRecipientViewModel()
+            {
+                EmailAddress = "tt@tt.tt",
+                BirthDate = DateTime.Now,
+                Gender = "Male",
+                KeyWords = "add",
+                Name = "John",
+                Surname = "Snow",
+                Priority = "Low"
+            });
+            mockMapper.Setup(m => m.Map<EmailRecipientViewModel, EmailRecipient>(It.IsAny<EmailRecipientViewModel>()))
+                .Returns(new EmailRecipient() { BirthDate = DateTime.Now, Gender = 0, KeyWords = "add", Name = "John", Surname = "Snow", Priority = "Low" });
+            var result = manager.IncertWithRecepients(emptyCampaign, recepientsList);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Insert_CampignWithWrongRecipientMapping_FalseResult()
+        {
+            EmailCampaignViewModel emptyCampaign = new EmailCampaignViewModel() { EmailAddress = "", Name = "test", Message = "test", };
+            List<EmailRecipientViewModel> recepientsList = new List<EmailRecipientViewModel>();
+            List<Email> Emails = new List<Email>();
+            Emails.Add(
+                new Email()
+                {
+                    Id = 10,
+                    EmailAddress = "qq@qq.qq"
+                });
+            Emails.Add(
+                new Email()
+                {
+                    Id = 11,
+                    EmailAddress = "tt@tt.tt"
+                });
+            mockUnitOfWork.Setup(u => u.Emails.Get(It.IsAny<Expression<Func<Email, bool>>>(), It.IsAny<Func<IQueryable<Email>,
+                IOrderedQueryable<Email>>>(), It.IsAny<string>())).Returns(new List<Email>());
+            mockUnitOfWork.Setup(u => u.EmailCampaigns.Insert(It.IsAny<EmailCampaign>()));
+            mockUnitOfWork.Setup(u => u.EmailRecipients.Insert(It.IsAny<EmailRecipient>()));
+            mockUnitOfWork.Setup(u => u.Emails.Insert(It.IsAny<Email>()));
+            mockMapper.Setup(m => m.Map<EmailCampaign>(It.IsAny<EmailCampaignViewModel>()))
+                .Returns(new EmailCampaign() { Name = "test", Message = "test" });
+            recepientsList.Add(new EmailRecipientViewModel()
+            {
+                EmailAddress = "tt@tt.tt",
+                BirthDate = DateTime.Now,
+                Gender = "Male",
+                KeyWords = "add",
+                Name = "John",
+                Surname = "Snow",
+                Priority = "Low"
+            });
+            var result = manager.IncertWithRecepients(emptyCampaign, recepientsList);
+            Assert.IsFalse(result);
+        }
+    }
+}
--- SmsServiceApp/BAL/Managers/EmailCampaignManager.cs
+++ SmsServiceApp/BAL/Managers/EmailCampaignManager.cs
@@ -48,96 +48,93 @@ namespace BAL.Managers
             return unitOfWork.EmailCampaigns.Get(ec => ec.UserId == userId && ec.Name.Contains(searchValue)).Count();
         }
 
-        public void Insert(EmailCampaignViewModel item)
+        public bool Update(EmailCampaignViewModel item)
         {
-            EmailCampaign emailCampaign = mapper.Map<EmailCampaign>(item);
-            Email email = unitOfWork.Emails.Get(filter: e => e.EmailAddress == item.EmailAddress).FirstOrDefault();
-            if (email == null)
+            try
             {
-                email = new Email();
-                email.EmailAddress = item.EmailAddress;
-                unitOfWork.Emails.Insert(email);
-                emailCampaign.Email = email;
-            }
-            else
-            {
-                emailCampaign.EmailId = email.Id;
-            }
-            notificationsGenerator.SupplyWithCampaignNotifications(emailCampaign);
-            unitOfWork.EmailCampaigns.Insert(emailCampaign);
-            unitOfWork.Save();
-        }
-
-        public int InsertWithId(EmailCampaignViewModel item)
-        {
-            EmailCampaign company = mapper.Map<EmailCampaign>(item);
-            notificationsGenerator.SupplyWithCampaignNotifications(company);
-            int id = unitOfWork.EmailCampaigns.InsertWithId(company);
-            return id;
-        }
-
-        public void Update(EmailCampaignViewModel item)
-        {
-            EmailCampaign emailCampaign = mapper.Map<EmailCampaign>(item);
-            Email email = unitOfWork.Emails.Get(filter: e => e.EmailAddress == item.EmailAddress).FirstOrDefault();
-            if (email == null)
-            {
-                email = new Email();
-                email.EmailAddress = item.EmailAddress;
-                unitOfWork.Emails.Insert(email);
-                emailCampaign.Email = email;
-            }
-            else
-            {
-                emailCampaign.EmailId = email.Id;
-            }
-            unitOfWork.EmailCampaigns.Update(emailCampaign);
-            unitOfWork.Save();
-        }
-
-        public void Delete(int id)
-        {
-            EmailCampaign emailCampaign = unitOfWork.EmailCampaigns.GetById(id);
-            unitOfWork.EmailCampaigns.Delete(emailCampaign);
-            unitOfWork.Save();
-        }
-
-        public void IncertWithRecepients(EmailCampaignViewModel campaign, List<EmailRecipientViewModel> emailRecipients)
-        {
-            EmailCampaign emailCampaign = mapper.Map<EmailCampaign>(campaign);
-            Email email = unitOfWork.Emails.Get(filter: e => e.EmailAddress == campaign.EmailAddress).FirstOrDefault();
-            if (email == null)
-            {
-                email = new Email();
-                email.EmailAddress = campaign.EmailAddress;
-                unitOfWork.Emails.Insert(email);
-                emailCampaign.Email = email;
-            }
-            else
-            {
-                emailCampaign.EmailId = email.Id;
-            }
-            notificationsGenerator.SupplyWithCampaignNotifications(emailCampaign);
-            unitOfWork.EmailCampaigns.Insert(emailCampaign);
-            unitOfWork.Save();
-            foreach (var recipient in emailRecipients)
-            {
-                EmailRecipient newRecepient = mapper.Map<EmailRecipientViewModel, EmailRecipient>(recipient);
-                newRecepient.CompanyId = emailCampaign.Id;
-                email = unitOfWork.Emails.Get(filter: e => e.EmailAddress == recipient.EmailAddress).FirstOrDefault();
+                EmailCampaign emailCampaign = mapper.Map<EmailCampaign>(item);
+                Email email = unitOfWork.Emails.Get(filter: e => e.EmailAddress == item.EmailAddress).FirstOrDefault();
                 if (email == null)
                 {
                     email = new Email();
-                    email.EmailAddress = recipient.EmailAddress;
+                    email.EmailAddress = item.EmailAddress;
                     unitOfWork.Emails.Insert(email);
-                    newRecepient.Email = email;
+                    emailCampaign.Email = email;
                 }
                 else
                 {
-                    newRecepient.EmailId = email.Id;
+                    emailCampaign.EmailId = email.Id;
                 }
-                unitOfWork.EmailRecipients.Insert(newRecepient);
+                unitOfWork.EmailCampaigns.Update(emailCampaign);
                 unitOfWork.Save();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                EmailCampaign emailCampaign = unitOfWork.EmailCampaigns.GetById(id);
+                unitOfWork.EmailCampaigns.Delete(emailCampaign);
+                unitOfWork.Save();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool IncertWithRecepients(EmailCampaignViewModel campaign, List<EmailRecipientViewModel> emailRecipients)
+        {
+            try
+            {
+                EmailCampaign emailCampaign = mapper.Map<EmailCampaign>(campaign);
+                Email email = unitOfWork.Emails.Get(filter: e => e.EmailAddress == campaign.EmailAddress).FirstOrDefault();
+                if (email == null)
+                {
+                    email = new Email();
+                    email.EmailAddress = campaign.EmailAddress;
+                    unitOfWork.Emails.Insert(email);
+                    emailCampaign.Email = email;
+                }
+                else
+                {
+                    emailCampaign.EmailId = email.Id;
+                }
+                notificationsGenerator.SupplyWithCampaignNotifications(emailCampaign);
+                unitOfWork.EmailCampaigns.Insert(emailCampaign);
+                unitOfWork.Save();
+                foreach (var recipient in emailRecipients)
+                {
+                    EmailRecipient newRecepient = mapper.Map<EmailRecipientViewModel, EmailRecipient>(recipient);
+                    newRecepient.CompanyId = emailCampaign.Id;
+                    email = unitOfWork.Emails.Get(filter: e => e.EmailAddress == recipient.EmailAddress).FirstOrDefault();
+                    if (email == null)
+                    {
+                        email = new Email();
+                        email.EmailAddress = recipient.EmailAddress;
+                        unitOfWork.Emails.Insert(email);
+                        newRecepient.Email = email;
+                    }
+                    else
+                    {
+                        newRecepient.EmailId = email.Id;
+                    }
+                    unitOfWork.EmailRecipients.Insert(newRecepient);
+                    unitOfWork.Save();
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
             }
         }        
     }
