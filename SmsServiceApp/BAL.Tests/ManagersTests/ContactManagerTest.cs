ca7f9bae8d1cbf834461df3aaf911c77b34d167c
Pasha Ivasiuta <pash.ok13@yandex.ua>
2019-04-19 16:12:54 +0300

    Add test for contact manager

--- SmsServiceApp/BAL.Tests/ManagersTests/ContactManagerTest.cs
+++ SmsServiceApp/BAL.Tests/ManagersTests/ContactManagerTest.cs
@@ -4,6 +4,8 @@ using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using WebApp.Models;
 
@@ -37,7 +39,7 @@ namespace BAL.Tests.ManagersTests
         [Test]
         public void GetContact_ExistingId_Contact()
         {
-             var contact = new Contact();
+            var contact = new Contact();
             var contactViewModel = new ContactViewModel();
             int Id = 10;
             mockUnitOfWork.Setup(x => x.Contacts.GetById(It.Is<int>(c => c == Id))).Returns(contact);
@@ -48,6 +50,90 @@ namespace BAL.Tests.ManagersTests
 
         }
 
-       
-   }
+        [Test]
+        public void GetContact_InvalidData_EmptyResult()
+        {
+			mockUnitOfWork.Setup(m => m.Contacts.GetContactsByPageNumber(1,1, It.IsAny<Expression<Func<Contact, bool>>>(), It.IsAny<Func<IQueryable<Contact>,
+				IOrderedQueryable<Contact>>>())).Returns(new List<Contact>());
+
+			mockMapper.Setup(c => c.Map<IEnumerable<Contact>, List<ContactViewModel>>(new List<Contact>()))
+				.Returns(new List<ContactViewModel>());
+
+			var result = manager.GetContact(-1, 1, 1);
+
+			Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void GetContact_ValidData_ContactCollection()
+        {
+	        Contact contact = new Contact() { Name = "Name", Id = 10, PhoneId = 1};
+	        mockUnitOfWork.Setup(m => m.Contacts.GetContactsByPageNumber(1, 1, It.IsAny<Expression<Func<Contact, bool>>>(), It.IsAny<Func<IQueryable<Contact>,
+		        IOrderedQueryable<Contact>>>())).Returns(new List<Contact>(){ contact });
+	        mockUnitOfWork.Setup(m => m.Phones.GetById(1)).Returns(new Phone());
+
+	        mockMapper.Setup(c => c.Map<IEnumerable<Contact>, List<ContactViewModel>>(It.IsAny<List<Contact>>()))
+		        .Returns(new List<ContactViewModel>(){new ContactViewModel(){Name = "name"}});
+
+	        var result = manager.GetContact(1, 1, 1);
+
+	        Assert.That(result, Is.Not.Empty);
+			Assert.That(result, Is.TypeOf<List<ContactViewModel>>());
+        }
+
+        [Test]
+        public void GetContactBySearchValue_EmptyValue_EmptyResult()
+        {
+			List<Contact> testContactList = new List<Contact>(){new Contact(){Name = "name"}, new Contact(){Name = "a"}};
+
+			mockUnitOfWork.Setup(m => m.Contacts.GetAll()).Returns(testContactList);
+			mockUnitOfWork.Setup(m => m.Phones.GetById(1)).Returns(new Phone() {PhoneNumber = "0"});
+			mockMapper.Setup(c => c.Map<IEnumerable<Contact>, List<ContactViewModel>>(It.IsAny<List<Contact>>()))
+				.Returns(new List<ContactViewModel>());
+
+			var result = manager.GetContactBySearchValue(1,1,1,"");
+
+			Assert.That(result, Is.Null);
+        }
+
+		//[Test]
+		//public void GetContactBySearchValue_ValidSearchValue_NonEmptyCollection()
+		//{
+		//	List<Contact> testContactList = new List<Contact>() { new Contact() { KeyWords = "name", ApplicationGroupId = 1 }, new Contact() { KeyWords = "a", ApplicationGroupId = 1 } };
+
+		//	mockUnitOfWork.Setup(m => m.Contacts.GetAll()).Returns(testContactList);
+		//	mockUnitOfWork.Setup(m => m.Phones.GetById(1)).Returns(new Phone() { PhoneNumber = "0" });
+		//	mockMapper.Setup(c => c.Map<IEnumerable<Contact>, List<ContactViewModel>>(It.IsAny<List<Contact>>()))
+		//		.Returns(new List<ContactViewModel>());
+
+		//	var result = manager.GetContactBySearchValue(1, 1, 1, "a");
+
+		//	Assert.That(result, Is.Not.Null);
+		//}
+
+		[Test]
+		public void GetContactCount_InvalidId_NullResult()
+		{
+			mockUnitOfWork
+				.Setup(m =>m.Contacts.Get(It.IsAny<Expression<Func<Contact, bool>>>(), null, ""))
+				.Returns(new List<Contact>());
+
+			var result = manager.GetContactCount(-1);
+
+			Assert.That(result, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void GetContactCount_ValidData_CountAreMoreThenZero()
+		{
+			mockUnitOfWork
+				.Setup(m => m.Contacts.Get(It.IsAny<Expression<Func<Contact, bool>>>(), null, ""))
+				.Returns(new List<Contact>(){new Contact()});
+
+			var result = manager.GetContactCount(1);
+
+			Assert.That(result, Is.Not.EqualTo(0));
+			Assert.That(result, Is.EqualTo(1));
+		}
+	}
 }
